0e46bb9f397b3a1130f39258b59f4a749dee39a6
xprilion <xprilion@gmail.com>
2017-03-09 19:39:58 +0530

    Solving Issue #2
    Partially moved scripts and styles to respective files

--- js/script.js
+++ js/script.js
@@ -20,8 +20,43 @@ var run = function(){
   }
 }
 
-
+var menuopen=0;
 $(document).ready(function(){
+	
 	particlesJS.load('home', 'js/particles-home.json');
+	
+	$("#load").click(function(){
+        $("#gallery").slideToggle("slow");
+    });
+	
+	
+    $("#menubar").click(function(){
+        
+		if(!menuopen){
+			$("#menu").show();
+			$('.menupanel').each(function(){
+				$(this).removeClass('width0').addClass('width100');
+			});
+			menuopen=1;
+		}
+		else{
+			$('.menupanel').each(function(){
+				$(this).removeClass('width100').addClass('width0');
+			});
+			
+			setTimeout(function(){
+				$("#menu").hide();
+			}, 550);
+			menuopen=0;
+		}
+    });
 });
-//window.setInterval(run, 500);
+
+function myMap() {
+				  var myCenter = new google.maps.LatLng(22.475730, 88.414632);
+				  var mapCanvas = document.getElementById("map");
+				  var mapOptions = {center: myCenter, zoom: 16};
+				  var map = new google.maps.Map(mapCanvas, mapOptions);
+				  var marker = new google.maps.Marker({position:myCenter});
+				  marker.setMap(map);
+				}
